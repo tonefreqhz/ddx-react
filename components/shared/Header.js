6b2c560ab6516a4b45a45baaddb13ce2d3b3aacf
orpheus <ryanachacon@gmail.com>
2019-03-07 10:49:45 -0800

    finish/add responsive header menu

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Link from 'next/link'
 
 import {css} from '@emotion/core'
 import {flexRow} from '../../styles/shared/GeneralStyles'
-import theme from '../../styles/shared/Theme'
+import theme, {breakpoints} from '../../styles/shared/Theme'
 
 const maxq = theme.maxq //media query using max-width
 
@@ -25,7 +25,7 @@ const sectionLeft = css`
 	align-items: center;
 	margin-left: 50px;
 	${maxq['sm']} {
-		display: none;
+		margin-left: 25px;
 	};
 `
 
@@ -33,23 +33,62 @@ const sectionRight = css`
 	${flexRow};
 	justify-content: flex-end;
 	margin-right: 50px;
+	align-content: center;
+	position: relative;
+	a {
+		display: flex;
+		align-items: center;
+		color: black;
+		text-decoration: none;
+		cursor: pointer;
+		&:hover {
+			text-decoration: underline;
+		};
+		margin: 0 10px;
+		${maxq['sm']} {
+			margin: 5px;
+		};
+	}
 	${maxq['sm']} {
 		flex-wrap: wrap;
 			margin: 0 20px;
 	};
 `
 
+const navMenuContainer = (openMenu) => {
+	let menuDisplay = openMenu ? 'flex' : 'none'
+	return css`
+		display: flex;
+		flex-direction: row;
+		img {
+			display: none;
+		}
+		${maxq['sm']} {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-end;
+			position: absolute;
+			top: -12px;
+			right: 10px;
+			z-index: 300;
+			width: 150px;
+			img {
+				display: flex;
+				margin-bottom: 10px;
+				cursor: pointer;
+			}
+			a {
+				display: ${menuDisplay};
+				padding: 2px 0;
+				width: 125px;
+			    justify-content: flex-end;
+			    background-color: white;
+			}
+		};
+	`
+}
+
 const navtag = css`
-	color: black;
-	text-decoration: none;
-	cursor: pointer;
-	&:hover {
-		text-decoration: underline;
-	};
-	margin: 0 10px;
-	${maxq['sm']} {
-		margin: 5px;
-	};
 `
 
 const circleImage = css`
@@ -68,6 +107,19 @@ const titleText = css`
 `
 
 const Header = () => {
+	const [openMenu, toggleMenu] = useState(false)
+	
+	useEffect(() => {
+		function handleResize() {
+			if (window.innerWidth > breakpoints['sm']) {
+				toggleMenu(false)
+			}
+		}
+		window.addEventListener('resize', handleResize)
+		return () => window.removeEventListener('resize', handleResize)
+	}, [])
+
+	const responsiveContainer = navMenuContainer(openMenu)
 	return <div css={headerContainer}>
 		<div css={sectionLeft}>
 			<Link href={'/'}>
@@ -78,21 +130,30 @@ const Header = () => {
 			</Link>
 		</div>
 		<div css={sectionRight}>
-			<Link href="/about" prefetch>
-				<a css={navtag}>About</a>
-			</Link>
-			<Link href="/browse" prefetch>
-				<a css={navtag}>Browse Database</a>
-			</Link>
-			<Link href="/featured" prefetch>
-				<a css={navtag}>Featured</a>
-			</Link>
-			<Link href="/challenges" prefetch>
-				<a css={navtag}>Challenges</a>
-			</Link>
-			<Link href="/contact" prefetch>
-				<a css={navtag}>Contact</a>
-			</Link>
+			<div css={responsiveContainer}>
+				<img
+					src={'/static/baseline-menu-24px.svg'} alt={'none'}
+					onClick={() => {
+						toggleMenu(!openMenu)}
+					}
+				/>
+				<Link href="/about" prefetch>
+					<a css={navtag}>About</a>
+				</Link>
+				<Link href="/browse" prefetch>
+					<a css={navtag}>Browse Database</a>
+				</Link>
+				<Link href="/featured" prefetch>
+					<a css={navtag}>Featured</a>
+				</Link>
+				<Link href="/challenges" prefetch>
+					<a css={navtag}>Challenges</a>
+				</Link>
+				<Link href="/contact" prefetch>
+					<a css={navtag}>Contact</a>
+				</Link>
+			</div>
+		
 		</div>
 	</div>
 }
