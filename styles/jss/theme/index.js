b74dc97b30bf32cf4b5b7b82e24465649c2d8514
orpheus <ryanachacon@gmail.com>
2019-03-30 09:51:52 -0700

    add jss/theme[provider]

---
+++ styles/jss/theme/index.js
@@ -0,0 +1,9 @@
+import {ThemeProvider} from 'react-jss/src/index'
+import ThemeGenerator from './ThemeGenerator'
+import useTheme from './useTheme'
+
+export {
+	ThemeProvider,
+	ThemeGenerator,
+	useTheme
+}
